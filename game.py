d352feedc017fc2abf0970394481a280696fa078
RsaDr0pyz <rsadr0pyz@gmail.com>
2023-10-28 16:28:50 -0300

    Adding animations

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from tkinter import *
+import time
 
 class SlotStates(Enum):
     EMPTY = 0,
@@ -9,9 +10,9 @@ class SlotStates(Enum):
 
 class Game(object):
     def __init__(self):
-        self.actualPlayer = SlotStates.P1
         self.slots = []
-        self.initSlot()
+        self.actualPlayer = SlotStates.P1
+        self.animating = False
 
     def initSlot(self):
         for i in range(6):
@@ -19,20 +20,44 @@ class Game(object):
             for l in range(7):
                 self.slots[i].append(SlotStates.EMPTY)
 
-    def placeInSlot(self, col):
+    def placeInSlot(self, col, canvas:Canvas, window, velocity):
+        self.animating = True
+        circleRadius = 100
+        circleStartY = 0
+        circleFinalY = 0
+
         for i in range(5, -1, -1):
             if( self.slots[i][col] == SlotStates.EMPTY):
+                circleFinalY = i*circleRadius
                 self.slots[i][col] = self.actualPlayer
                 break
+
+        posY = circleStartY*circleRadius
+        posX = col*circleRadius
+        posFY = posY+circleRadius
+        posFX = posX + circleRadius
+
+        fill = ""
+        if self.actualPlayer == SlotStates.P1:
+            fill = "red"
+        else:
+            fill = "blue"
+
+        circle = canvas.create_oval(posX, posY, posFX, posFY, fill=fill)
+        actualY = circleStartY
+
+        refreshTime = 1/60 #60 fps
+        yStep = velocity*refreshTime
+        while(actualY < circleFinalY):
+            canvas.move(circle, 0, yStep)
+            window.update()
+            time.sleep(refreshTime)
+            actualY += yStep 
+        canvas.moveto(circle, y=circleFinalY)
+        self.animating = False
         self.switchPlayer()
     
-    def switchPlayer(self):
-        if(self.actualPlayer == SlotStates.P1):
-            self.actualPlayer = SlotStates.P2
-        else:
-            self.actualPlayer = SlotStates.P1
-    
-    def updateCanvas(self, canvas:Canvas):
+    def drawCanvas(self, canvas:Canvas):
         canvas.delete('all')
         circleBorder = 3
         circleH = (canvas.winfo_height() - 2*circleBorder)/6
@@ -52,3 +77,9 @@ class Game(object):
                     fill = "blue"
                 
                 canvas.create_oval(posX, posY, posFX, posFY, width=circleBorder, fill=fill)
+    
+    def switchPlayer(self):
+        if(self.actualPlayer == SlotStates.P1):
+            self.actualPlayer = SlotStates.P2
+        else:
+            self.actualPlayer = SlotStates.P1
--- lig4.py
+++ lig4.py
@@ -6,13 +6,12 @@ bttFrame = Frame(root)
 bttFrame.grid()
 
 lig4 = Game()
+lig4.initSlot()
 
 canvasFrame = Frame(root)
 canvasFrame.grid(column=0, row=1)
 canv = Canvas(canvasFrame, width=700, height=600, bg="white")
 canv.grid(column=0, row=1)
-
-
 
 for l in range(7):
     f = Frame(bttFrame, height=32, width=100)
@@ -20,9 +19,11 @@ for l in range(7):
 
     btt = Button(f, text="O", padx=0, width=1)
     btt.place(x=0, y=0, relheight=1, relwidth=1)
+
     def bttFunc(event, l=l):
-        lig4.placeInSlot(l)
-        lig4.updateCanvas(canv)
+        if(not lig4.animating):
+            lig4.placeInSlot(l, canv, root, 600)
+
     btt.bind("<ButtonPress>", bttFunc)
 
 
