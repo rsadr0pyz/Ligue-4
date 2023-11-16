b7d70c591e1bf8a2c08cceb4ff697b8abce01cdc
heitorps <heitorps2020@gmail.com>
2023-11-16 12:25:02 -0300

    esp mqtt mensagem enviada

--- game.ino
+++ game.ino
@@ -1,3 +1,95 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+// ###################### Trocar aqui!!! ##################### 
+const char* ssid = "PIC2-2.4G";
+const char* password = "engcomp@ufes";
+//Free mqtt server for testing
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+//Local MQTT server - Tested using mosquitto mqtt for windows and linux
+//const char* mqtt_server = "192.168.137.1";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+  delay(10);
+  
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("Ligue4PIC", "de quatro!!");
+    } 
+    else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+     reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    value = random(20,50);
+    snprintf (msg, 50, "%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("Ligue4PIC", msg);
+  }
+}
+
 #define MAXLIN 6
 #define MAXCOL 7
 #define VAZIO ' '
@@ -20,10 +112,13 @@ int nJogadas = MAXLIN*MAXCOL;
 void setup(){
   InicializaTabuleiro(tabuleiro);
   Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 char jogador = JOGADOR1;
-void loop(){
+/* void loop(){
 
   
   if(Serial.available()>0){
@@ -47,7 +142,7 @@ void loop(){
     //Serial.println("CORINTHIANS");
   }
   
-}
+} */
 
 
 void InicializaTabuleiro(char tabuleiro[MAXLIN][MAXCOL]){
